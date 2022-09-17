c1372d7b5dc4418d81883d2ead85a62717a4c838
rygelg <112190626+rygelg@users.noreply.github.com>
2022-09-17 18:27:52 -0400

    Update getLyrics.py

--- getLyrics.py
+++ getLyrics.py
@@ -5,17 +5,17 @@ from lyricsgenius import Genius
 genius = Genius("nBPoYQZo2F3Pn0e4mO7FrCiPkJLLJSXgMKhbmkgAbRFwdfNCDQ5j90Gyi-USRy4H")
 
 def findChorus(geniusLyrics):
-    chorusStart = geniusLyrics.find("Chorus") #int for first instance of Chrous
+    chorusStart = geniusLyrics.find("[Chorus]") #int for first instance of Chrous
     chorusString = geniusLyrics[chorusStart:]#substring that starts at first Chorus
     chorus = ""
-    lines = chorusString.splitlines(True)
+    lines = chorusString.splitlines()
     for line in lines:
         if len(line.strip()) == 0:
             break
-        elif line.startswith("["):
+        elif line.startswith("[") and not (line.startswith("[Chorus]")):
             break
         else:
-            chorus += line
+            chorus += line + "\n"
 
     return chorus
 
@@ -24,6 +24,7 @@ userInput = input()
 song = genius.search_song(userInput)
 
 print ("Results for: " + userInput + " by " + song.artist)
+
 
 geniusLyrics = song.to_text()
 
